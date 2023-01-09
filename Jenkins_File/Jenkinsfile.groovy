8e086d492e4ec872e5f420ea5c7265d2faab0bf1
DinGolan <dingolan100@gmail.com>
2023-01-09 18:38:14 +0200

    Update - Jenkins File

--- Jenkins_File/Jenkinsfile.groovy
+++ Jenkins_File/Jenkinsfile.groovy
@@ -1,7 +1,13 @@
 pipeline {
     agent any
+
+    parameters {
+        choice(name: 'User_Choice', choices: ['1', '2', '3'], description: "The ${User} need to choose a number : \n" + "1 - In case the int value is 1 – only frontend_testing.py will run.\n 2 - In case the int value is 2 – only backend_testing.py will run.\n 3 - In case the int value is 3 – only combined_testing.py will run.\n")
+    }
     stages {
         // Step 1 - Clone Git From GitHub //
+        // Note   - This section is deprecated because of the 'Extra' section //
+        /*
         stage("Clone Git") {
             steps {
                 script {
@@ -10,6 +16,7 @@ pipeline {
                 git "https://github.com/DinGolan/DevOps-Experts-Final-Project.git"
             }
         }
+        */
 
         // Step 2 - Run REST API //
         stage("Run `rest_app.py` (Backend)") {
@@ -18,39 +25,50 @@ pipeline {
             }
         }
 
-        // Step 3 - Run WEB APP //
+        // Step 3 - Run WEB APP  //
         stage("Run `web_app.py` (Frontend)") {
             steps {
                 bat 'start /min python web_app.py'
             }
         }
 
-        // Step 4 - Run Backend Testing //
-        stage("Run `backend_testing.py` (Testing)") {
+        // Step 4 - Run Testings //
+        stage("Testing") {
             steps {
-                bat 'python backend_testing.py'
+                script {
+                    if (User_Choice == '1') {
+                        bat 'echo Run `backend_testing.py` (Testing)'
+                        bat 'python backend_testing.py'
+                    } else if (User_Choice == '2') {
+                        bat 'echo Run `frontend_testing.py` (Testing)'
+                        bat 'python frontend_testing.py'
+                    } else if (User_Choice == '3') {
+                        bat 'echo Run `combined_testing.py` (Testing)'
+                        bat 'python combined_testing.py'
+                    }
+                }
+
             }
         }
 
-        // Step 5 - Run Frontend Testing //
-        stage("Run `frontend_testing.py` (Testing)") {
-            steps {
-                bat 'python frontend_testing.py'
-            }
-        }
-
-        // Step 6 - Run Combined Testing //
-        stage("Run `combined_testing.py` (Testing)") {
-            steps {
-                bat 'python combined_testing.py'
-            }
-        }
-
-        // Step 7 - Run Clean Environment //
+        // Step 5 - Run Clean Environment //
         stage("Run `clean_environment.py` (Clean)") {
             steps {
                 bat 'python clean_environment.py'
             }
         }
     }
+
+    // Step - Sending Mail //
+    // Note - This section is not supported because I've Two Factor Authentication Gmail //
+    /*
+    post {
+        failure {
+            mail to: "dingolan100@gmail.com",
+            subject: "DevOps Experts - Final Project - Updates",
+            body: "${currentBuild.currentResult} : Job ${env.JOB_NAME}\nMore Info can be found here : ${env.BUILD_URL}\n"
+            attachLog: true
+        }
+    }
+    */
 }
