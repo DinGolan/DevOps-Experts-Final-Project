f5a2176a742651414e967ba1b3026271505b0029
DinGolan <dingolan100@gmail.com>
2023-01-09 12:37:01 +0200

    Add - Jenkins File

---
+++ Jenkins_File/Jenkinsfile.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+    stages {
+        // Step 1 - Clone Git From GitHub //
+        stage("Clone Git") {
+            steps {
+                // TODO - git "https://github.com/DinGolan/DevOps-Experts-Final-Project.git"
+            }
+        }
+
+        // Step 2 - Run REST API //
+        stage("Run `rest_app.py` (Backend)") {
+            steps {
+                bat 'start /min python rest_app.py'
+            }
+        }
+
+        // Step 3 - Run WEB APP //
+        stage("Run `web_app.py` (Frontend)") {
+            steps {
+                bat 'start /min python web_app.py'
+            }
+        }
+
+        // Step 4 - Run Backend Testing //
+        stage("Run `backend_testing.py` (Testing)") {
+            steps {
+                bat 'python backend_testing.py'
+            }
+        }
+
+        // Step 5 - Run Frontend Testing //
+        stage("Run `frontend_testing.py` (Testing)") {
+            steps {
+                bat 'python frontend_testing.py'
+            }
+        }
+
+        // Step 6 - Run Combined Testing //
+        stage("Run `combined_testing.py` (Testing)") {
+            steps {
+                bat 'python combined_testing.py'
+            }
+        }
+
+        // Step 7 - Run Clean Environment //
+        stage("Run `clean_environment.py` (Clean)") {
+            steps {
+                bat 'python clean_environment.py'
+            }
+        }
+    }
+}
