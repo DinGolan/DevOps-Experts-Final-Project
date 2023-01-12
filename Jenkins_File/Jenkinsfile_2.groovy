9eb24f50b8574d59029d6c18751ed4f123d7404c
DinGolan <dingolan100@gmail.com>
2023-01-12 12:29:44 +0200

    Update - Jenkins File

--- Jenkins_File/Jenkinsfile_2.groovy
+++ Jenkins_File/Jenkinsfile_2.groovy
@@ -91,13 +91,13 @@ pipeline {
                         def user_choice = bat(script: 'echo %User_Choice%', returnStdout: true).trim()
                         echo "user_choice : ${user_choice}"
 
-                        if (user_choice == '1') {
+                        if (user_choice == 1) {
                             bat 'echo Run `backend_testing.py` (Testing)'
                             bat 'python Testing\\backend_testing.py -u %DB_USER_NAME% -p %DB_PASSWORD% -i %IS_JOB_RUN% -r %REQUEST_TYPE%'
-                        } else if (user_choice == '2') {
+                        } else if (user_choice == 2) {
                             bat 'echo Run `frontend_testing.py` (Testing)'
                             bat 'python Testing\\frontend_testing.py -u %DB_USER_NAME% -p %DB_PASSWORD% -i %IS_JOB_RUN%'
-                        } else if (user_choice == '3') {
+                        } else if (user_choice == 3) {
                             bat 'echo Run `combined_testing.py` (Testing)'
                             bat 'python Testing\\combined_testing.py -u %DB_USER_NAME% -p %DB_PASSWORD% -i %IS_JOB_RUN% -r %REQUEST_TYPE% -t %TEST_SIDE%'
                         } else {
