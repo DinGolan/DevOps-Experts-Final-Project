db3842d5c0daf6631fb0d9596d5c57df18e0adb8
DinGolan <dingolan100@gmail.com>
2023-02-09 18:22:29 +0200

    Update - Docker Backend Testing

--- Testing/docker_backend_testing.py
+++ Testing/docker_backend_testing.py
@@ -150,7 +150,7 @@ def send_post_request(user_name, rest_host):
     print("##########\n")
     new_user_id     = get_new_user_id_from_users_table(isDocker=True)
     url             = f"http://{rest_host}:{get_rest_port()}/{get_db_users_table_name()}/{new_user_id}"
-    requests_result = requests.post(url=url, json={"user_name": user_name, "isDocker": True})
+    requests_result = requests.post(url=url, json={"user_name": user_name, "isDocker": "True"})
     json_result     = requests_result.json()
     check_requests_result("POST", new_user_id, requests_result, json_result, "user_added")
 
@@ -168,7 +168,7 @@ def send_get_request(url, user_id):
     print("\n#########")
     print("#  GET  #")
     print("#########\n")
-    requests_result = requests.get(url=url, json={"user_id": user_id, "isDocker": True})
+    requests_result = requests.get(url=url, json={"user_id": user_id, "isDocker": "True"})
     json_result     = requests_result.json()
     check_requests_result("GET", user_id, requests_result, json_result, "user_name")
 
@@ -185,7 +185,7 @@ def send_get_all_request(url):
     print("\n#############")
     print("#  GET ALL  #")
     print("#############\n")
-    requests_result = requests.get(url=url, json={"isDocker": True})
+    requests_result = requests.get(url=url, json={"isDocker": "True"})
     json_result     = requests_result.json()
     check_requests_result_for_get_all("GET_ALL", requests_result, json_result, "users_table")
 
@@ -215,7 +215,7 @@ def send_put_request(is_job_run, url, user_id, test_name):
     else:
         new_user_name = input("Please type new user name : ")
 
-    requests_result = requests.put(url=url, json={"user_id": user_id, "new_user_name": new_user_name, "isDocker": True})
+    requests_result = requests.put(url=url, json={"user_id": user_id, "new_user_name": new_user_name, "isDocker": "True"})
     json_result     = requests_result.json()
     check_requests_result("PUT", user_id, requests_result, json_result, "user_updated")
 
@@ -233,7 +233,7 @@ def send_delete_request(url, user_id):
     print("\n############")
     print("#  DELETE  #")
     print("############\n")
-    requests_result = requests.delete(url=url, json={"user_id": user_id, "isDocker": True})
+    requests_result = requests.delete(url=url, json={"user_id": user_id, "isDocker": "True"})
     json_result     = requests_result.json()
     check_requests_result("DELETE", user_id, requests_result, json_result, "user_deleted")
 
